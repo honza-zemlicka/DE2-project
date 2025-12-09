20b231ce1b1c32142bf3c57183cbc92e39b58a2b
vojtakrehan <256513@vutbr.cz>
2025-12-09 20:53:12 +0100

    Doxygen documentation

---
+++ Doxygen_html/dir_97aefd0d527b934f1d99a682da8fe6a9.js
@@ -0,0 +1,8 @@
+var dir_97aefd0d527b934f1d99a682da8fe6a9 =
+[
+    [ "gpio", "dir_541432474389f3d0c2d8c34c147b933c.html", "dir_541432474389f3d0c2d8c34c147b933c" ],
+    [ "oled", "dir_43acfda8ae2a7dc79d6ffc7268040542.html", "dir_43acfda8ae2a7dc79d6ffc7268040542" ],
+    [ "twi", "dir_7d92855bde7db39b1dfe3b8f27819bb3.html", "dir_7d92855bde7db39b1dfe3b8f27819bb3" ],
+    [ "uart", "dir_b4d6f175b33edcf901b87ee260cccf25.html", "dir_b4d6f175b33edcf901b87ee260cccf25" ],
+    [ "ultrasound", "dir_a1c52324d412f52ef0bc4848041a3262.html", "dir_a1c52324d412f52ef0bc4848041a3262" ]
+];
---
+++ Doxygen_html/topics.js
@@ -0,0 +1,7 @@
+var topics =
+[
+    [ "Timer Library &lt;timer.h&gt;", "group__fryza__timer.html", "group__fryza__timer" ],
+    [ "GPIO Library &lt;gpio.h&gt;", "group__fryza__gpio.html", "group__fryza__gpio" ],
+    [ "TWI Library &lt;twi.h&gt;", "group__fryza__twi.html", "group__fryza__twi" ],
+    [ "UART Library &lt;uart.h&gt;", "group__pfleury__uart.html", "group__pfleury__uart" ]
+];
