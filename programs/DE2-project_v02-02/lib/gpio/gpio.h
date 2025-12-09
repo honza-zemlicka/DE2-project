dfbe0d61bef3be04ff7c70c96780c4319348e38a
vojtakrehan <256513@vutbr.cz>
2025-12-09 20:11:53 +0100

    Update gpio.h
    brief

--- programs/DE2-project_v02-02/lib/gpio/gpio.h
+++ programs/DE2-project_v02-02/lib/gpio/gpio.h
@@ -73,21 +73,48 @@ void gpio_write_high(volatile uint8_t *reg, uint8_t pin);
 uint8_t gpio_read(volatile uint8_t *reg, uint8_t pin);
 
 
+/**
+* @brief    Configure one input pin without pull-up resistor.  
+* @param    reg Address of Data Direction Register, such as &DDRB 
+* @param    pin Pin designaton in the interval 0 to 7
+* @return   none
+*/
 void gpio_mode_input_nopull(volatile uint8_t *reg, uint8_t pin);
 
-
+/**
+* @brief    Toggle one output pin value.
+* @param    reg Adress of Port Register, such as &PORTB
+* @param    pin Pin designation in the interval 0 to 7
+* @return   none
+*/
 void gpio_toggle(volatile uint8_t *reg, uint8_t pin);
 
-
+/**
+* @brief    Initialize ADC.
+* @return   none
+*/
 void adc_init();
 
-
+/**
+* @brief    Read an analog value from a specific channel.
+* @param    pin Analog channel designation in the interval 0 to 5
+* @return   10-bit digital value
+*/
 uint16_t analog_read(uint8_t pin);
 
-
+/**
+* @brief    Initialize PWM generator on Timer0 and Timer1.
+* @return   none
+*/
 void pwm_init();
 
-
+/**
+* @brief    Write PWM to specific pin.
+* @param    reg Address 
+* @param    pin Pin designation in the interval 0 to 7
+* @param    val PWM duty cycle (0 to 255)
+* @return   none
+*/
 void pwm_write(volatile uint8_t *reg, uint8_t pin, uint8_t val);
 
 
