8838c3016edef7dc52b1957e244206014c654788
vojtakrehan <256513@vutbr.cz>
2025-12-09 21:53:39 +0100

    Doxygen update

--- Doxygen_html/dir_a1c52324d412f52ef0bc4848041a3262.js
+++ Doxygen_html/dir_a1c52324d412f52ef0bc4848041a3262.js
@@ -1,4 +1,4 @@
 var dir_a1c52324d412f52ef0bc4848041a3262 =
 [
-    [ "ultrasound.h", "ultrasound_8h_source.html", null ]
+    [ "ultrasound.h", "ultrasound_8h.html", "ultrasound_8h" ]
 ];
---
+++ Doxygen_html/group___ultrasound.js
@@ -0,0 +1,5 @@
+var group___ultrasound =
+[
+    [ "ultrasound_init", "group___ultrasound.html#ga6f942e82803982b546923243b3efc8a0", null ],
+    [ "ultrasound_read", "group___ultrasound.html#gac78a99cf1102a47e19f50a67b1856f59", null ]
+];
